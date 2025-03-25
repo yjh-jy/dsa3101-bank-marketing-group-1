00bd8a0ba7d52899b84582f9a2548adff764b0e5
jolene-ho <jolenehxe@gmail.com>
2025-03-25 22:30:22 +0800

    Added brief function explanations for load_data.py for clarity.

--- customer_engagement/load_data.py
+++ customer_engagement/load_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def load_customer_data():
+    '''
+    Loads csv files for customer-level engagement analysis
+    '''
     engagement_details = pd.read_csv("../data/processed/engagement_details.csv")
     customers = pd.read_csv("../data/processed/customer.csv")
     digital_usage = pd.read_csv("../data/processed/digital_usage.csv")
@@ -9,6 +12,9 @@ def load_customer_data():
     return engagement_details, customers, digital_usage, products_owned, transactions
 
 def load_campaign_data():
+    '''
+    Loads csv files for campaign-level engagement analysis
+    '''
     engagement_details = pd.read_csv("../data/processed/engagement_details.csv")
     campaigns = pd.read_csv("../data/processed/campaigns.csv")
     return engagement_details, campaigns
