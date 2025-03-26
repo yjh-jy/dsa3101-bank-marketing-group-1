75110870b94b1b1364ad8ef2a65f82ce3e8828f7
rooderich <roderichsuwandi@gmail.com>
2025-03-26 20:42:11 +0800

    Fixed bugs

--- campaign_optimization/scripts/data_ingestion_initial.py
+++ campaign_optimization/scripts/data_ingestion_initial.py
@@ -13,9 +13,9 @@ DB_PORT = "5432"
 
 print("Script Path:", os.path.abspath(__file__))
 # Load Relevant Data
-customers = pd.read_csv("/app/data/processed/customer.csv")
-engagements = pd.read_csv("/app/data/processed/engagement_details.csv")
-campaigns = pd.read_csv("/app/data/processed/campaigns.csv")
+customers = pd.read_csv("../app/data/processed/customer.csv")
+engagements = pd.read_csv("../app/data/processed/engagement_details.csv")
+campaigns = pd.read_csv("../app/data/processed/campaigns.csv")
 
 def categorize_income(income):
     if income < 3000:
